c354b21b97cbb371aa8b6ab69e4ae89870c89858
Téo Asseline <asselineteo@gmail.com>
2024-03-01 10:23:18 +0100

    Initialisation Test

--- src/BusinessSiteTest.java
+++ src/BusinessSiteTest.java
@@ -1,11 +1,91 @@
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Disabled;
+
+import java.util.Date;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BusinessSiteTest {
 
+	BusinessSite _subject;
+	
+	@SuppressWarnings("deprecation")
+	@BeforeEach
+	public void setUp() {
+		new Zone ("A", 0.06, 0.07, new Date ("15 May 2023"), new Date ("10 Sep 2023")).register();
+		new Zone ("B", 0.07, 0.06, new Date ("5 Jun 2023"), new Date ("31 Aug 2023")).register();
+		new Zone ("C", 0.065, 0.065, new Date ("5 Jun 2023"), new Date ("31 Aug 2023")).register();
+		_subject = new BusinessSite();
+	}
+
+	@SuppressWarnings("deprecation")
 	@Test
-	void testBusinessSite() {
-		assertAll();
+	public void testZero() {
+		_subject.addReading(new Reading (10, new Date ("1 Jan 2023")));
+		_subject.addReading(new Reading (10, new Date ("1 Feb 2023")));
+		assertEquals(0d, _subject.charge().amount());
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void test49() {
+		_subject.addReading(new Reading (59, new Date ("1 Jan 2023")));
+		_subject.addReading(new Reading (108, new Date ("1 Feb 2023")));
+		assertEquals(3.58d, _subject.charge().amount());
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void test50() {
+		_subject.addReading(new Reading (100, new Date ("1 Jan 2023")));
+		_subject.addReading(new Reading (150, new Date ("1 Feb 2023")));
+		assertEquals(3.66d, _subject.charge().amount());
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void test51() {
+		_subject.addReading(new Reading (1000, new Date ("1 Jan 2023")));
+		_subject.addReading(new Reading (1051, new Date ("1 Feb 2023")));
+		assertEquals(3.72d, _subject.charge().amount());
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void test74() {
+		_subject.addReading(new Reading (10000, new Date ("1 Jan 2023")));
+		_subject.addReading(new Reading (10074, new Date ("1 Feb 2023")));
+		assertEquals(5.39d, _subject.charge().amount());
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void test75() {
+		_subject.addReading(new Reading (0, new Date ("1 Jan 2023")));
+		_subject.addReading(new Reading (75, new Date ("1 Feb 2023")));
+		assertEquals(5.46d, _subject.charge().amount());
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void test76() {
+		_subject.addReading(new Reading (50, new Date ("1 Jan 2023")));
+		_subject.addReading(new Reading (126, new Date ("1 Feb 2023")));
+		assertEquals(5.53d, _subject.charge().amount());
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testMax() {
+		_subject.addReading(new Reading (0, new Date ("1 Jan 2023")));
+		_subject.addReading(new Reading (Integer.MAX_VALUE, new Date ("1 Feb 2023")));
+		assertEquals (1.5220290473E8, _subject.charge().amount());
+	}
+	
+	@Test
+	public void testNoReadings() {
+		try {
+			_subject.charge();
+			assert(false);
+		} catch (NullPointerException e) {}
 	}
 }
