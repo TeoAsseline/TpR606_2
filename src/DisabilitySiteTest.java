1236c0788281e28ef5fb8e83c187b895d9189007
Téo Asseline <asselineteo@gmail.com>
2024-03-01 10:02:31 +0100

    creation des classes test

---
+++ src/DisabilitySiteTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class DisabilitySiteTest {
+
+	@Test
+	void testDisabilitySite() {
+		assertAll();
+	}
+}
---
+++ src/ResidentialSiteTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class ResidentialSiteTest {
+
+	@Test
+	void testResidentialSite() {
+		assertAll();
+	}
+}
